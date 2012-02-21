1a4a534fa41be0c361e8172f5cef69105d28f732
Yalda <yalda.kolahdooz@gmail.com>
2012-02-21 18:55:31 +0100

    only the Change-N-to-M is left

--- FirstPythonProgram.py
+++ FirstPythonProgram.py
@@ -6,35 +6,39 @@ https://github.com/thehackerwithin/PyTrieste/wiki/ExerciseSession1-Basics
 import os
 import sys
 
+path="/afs/ictp.it/home/y/ykolahdo/ShellExample/thehackerwithin-PyTrieste-4f54727/shellExample/cleaneddata/alexander"
+
 ''' main '''
 def main():
 
   dirList = MakeFileNameList()
-  for fileName in dirList:
-    if LineContainsResponseN(fileName):
-      ChangeNtoM(fileName)
+  for fileName in dirList:    
+    index = LineContainsResponseN(fileName)
+    if index:      	
+      ChangeNtoM(fileName,index)
  
 
 
 ''' MakeFileNameList() finds all of the filenames and puts them in a list '''
 def MakeFileNameList():
-  path="/afs/ictp.it/home/y/ykolahdo/ShellExample/thehackerwithin-PyTrieste-4f54727/shellExample/cleaneddata/alexander"
   dirList=os.listdir(path)
   return dirList
 
 
 
 
-
 ''' LineContainsResponseN() checks if a given line contains a question with an N response '''
 def LineContainsResponseN(filename)
-  '''if contains'''
-  return True
+  
+  fileToCheck = path + filename
+  infile = open(fileToCheck,"r")
+  contents = infile.read()
+  infile.close()
 
-  ''' else '''
-  return False
-
-
+  searchTerm = "Sex: N"
+  index = contents.find(searchTerm)
+  
+  return index
 
 
 def ChangeNtoM(a_line)
