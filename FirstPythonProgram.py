f960bc2e246479288c79d6138f92ca1b15f93ba9
Yalda <yalda.kolahdooz@gmail.com>
2012-02-21 18:34:16 +0100

    the functions are defined but not the details :)

--- FirstPythonProgram.py
+++ FirstPythonProgram.py
@@ -3,31 +3,43 @@ This is my first python program.
 What it does is explained here: 
 https://github.com/thehackerwithin/PyTrieste/wiki/ExerciseSession1-Basics
 '''
-
+import os
+import sys
 
 ''' main '''
-def main()
+def main():
 
-  
+  dirList = MakeFileNameList()
+  for fileName in dirList:
+    if LineContainsResponseN(fileName):
+      ChangeNtoM(fileName)
  
-  return True
 
 
 ''' MakeFileNameList() finds all of the filenames and puts them in a list '''
-def MakeFileNameList()
+def MakeFileNameList():
+  path="/afs/ictp.it/home/y/ykolahdo/ShellExample/thehackerwithin-PyTrieste-4f54727/shellExample/cleaneddata/alexander"
+  dirList=os.listdir(path)
+  return dirList
+
+
+
 
 
 ''' LineContainsResponseN() checks if a given line contains a question with an N response '''
-def LineContainsResponseN()
+def LineContainsResponseN(filename)
   '''if contains'''
   return True
 
   ''' else '''
   return False
 
-def ChangeNtoM()
+
+
+
+def ChangeNtoM(a_line)
   
-  if LineContainsResponseN()
+  if LineContainsResponseN(a_line)
   ''' Change N in the line to M'''
   return True
 
