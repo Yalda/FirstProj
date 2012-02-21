ae6b667e17c4a399af90b884895c31dc117e8e47
Yalda <yalda.kolahdooz@gmail.com>
2012-02-21 18:01:30 +0100

    I am adding a first version

---
+++ FirstPythonProgram.py
@@ -0,0 +1,43 @@
+'''
+This is my first python program. 
+What it does is explained here: 
+https://github.com/thehackerwithin/PyTrieste/wiki/ExerciseSession1-Basics
+'''
+
+
+''' main '''
+def main()
+
+  
+ 
+  return True
+
+
+''' MakeFileNameList() finds all of the filenames and puts them in a list '''
+def MakeFileNameList()
+
+
+''' LineContainsResponseN() checks if a given line contains a question with an N response '''
+def LineContainsResponseN()
+  '''if contains'''
+  return True
+
+  ''' else '''
+  return False
+
+def ChangeNtoM()
+  
+  if LineContainsResponseN()
+  ''' Change N in the line to M'''
+  return True
+
+
+
+
+
+
+
+
+
+
+
